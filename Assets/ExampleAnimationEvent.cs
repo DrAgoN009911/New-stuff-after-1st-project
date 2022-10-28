a383697dca32444fb3818cb2c99983231a0b5e3f
Student <student@V120-ENT-008.local>
2022-10-28 17:42:54 -0400

    Adding animation event controller, multiple actions, multiple sprays.

---
+++ Assets/ExampleAnimationEvent.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kvant; 
+
+public class ExampleAnimationEvent : MonoBehaviour
+{
+
+    public GameObject spray;
+    Spray sprayParticle;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        sprayParticle = spray.GetComponent<Spray>();  
+    }
+
+    // Update is called once per frame
+    public void Spray(float duration)
+    {
+        Debug.Log("Spray Particle");
+        StartCoroutine(SprayForDuration(duration));
+    }
+
+
+
+    IEnumerator SprayForDuration(float duration)
+    {
+        sprayParticle.throttle = 0.5f;
+        Debug.Log("SprayForDuration " + duration);
+        yield return new WaitForSeconds(duration);
+        sprayParticle.throttle = 0.0f;
+    }
+    public void  PrintEvent(string s)
+    {
+        Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
+    }
+
+
+}
